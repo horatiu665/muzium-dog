d5a0e0f75ef574452ec2b3834c23177efafd7d74
Horatiu Roman <horatiuromantic@gmail.com>
2024-04-28 12:37:01 +0200

    replaced castle with newest version. something broke tho.

---
+++ Assets/WalkTheDog/Scripts/CastleRoadblock.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CastleRoadblock : MonoBehaviour
+{
+    // if player hits it, send a message.
+    
+}
