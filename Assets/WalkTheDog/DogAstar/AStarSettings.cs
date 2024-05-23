4a789ef2a621f3f6f0f1c04daa84aab7d09ad791
Horatiu Roman <horatiuromantic@gmail.com>
2024-05-23 17:20:56 +0200

    astar fix???

--- Assets/WalkTheDog/DogAstar/AStarSettings.cs
+++ Assets/WalkTheDog/DogAstar/AStarSettings.cs
@@ -7,7 +7,7 @@ public class AStarSettings : ScriptableObject
     public LayerMask layerMask = -1;
     public float nodeVerticalOffset = 0.1f;
 
-    public float neighborDistance = 5f;
+    public float neighborDistance = 5f; 
 
     public bool redoNeighborsOnPath = false;
 
